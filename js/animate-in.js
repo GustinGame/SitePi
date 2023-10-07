0e6cf534a47e1335dd541c01b798a8419187d4e1
GustinGame <gustavogoncalves3ch@gmail.com>
2023-10-06 21:17:50 -0300

    adding some alterations to the code

--- js/animate-in.js
+++ js/animate-in.js
@@ -7,30 +7,30 @@
  * Make sure to add this to the <head> of your page to avoid flickering on load 
  * Based on https://andycaygill.github.io/scroll-entrance/
  */
-(function() {
+(function () {
 
 	//Set up defaults
-  	var duration = "1000";
-  	var heightOffset = 100;
+	var duration = "500";
+	var heightOffset = 100;
 
 
-    // document.write("<style id='temp-animate-in'>*[class^='animate-in'], *[class*=' animate-in'] {display:none}</style>")
+	// document.write("<style id='temp-animate-in'>*[class^='animate-in'], *[class*=' animate-in'] {display:none}</style>")
 
-  	function isElementVisible(elem) {
+	function isElementVisible(elem) {
 
-      var rect = elem.getBoundingClientRect();
+		var rect = elem.getBoundingClientRect();
 
-      //Return true if any of the following conditions are met:
-      return (
-        // The top is in view: the top is more than 0 and less than the window height (the top of the element is in view)
-        ( (rect.top + heightOffset) >= 0 && (rect.top + heightOffset) <= window.innerHeight ) || 
-        // The bottom is in view: bottom position is greater than 0 and greater than the window height
-        ( (rect.bottom + heightOffset) >= 0 && (rect.bottom + heightOffset) <= window.innerHeight ) ||
-        // The top is above the viewport and the bottom is below the viewport
-        ( (rect.top + heightOffset) < 0 && (rect.bottom + heightOffset) > window.innerHeight )
-      )
+		//Return true if any of the following conditions are met:
+		return (
+			// The top is in view: the top is more than 0 and less than the window height (the top of the element is in view)
+			((rect.top + heightOffset) >= 0 && (rect.top + heightOffset) <= window.innerHeight) ||
+			// The bottom is in view: bottom position is greater than 0 and greater than the window height
+			((rect.bottom + heightOffset) >= 0 && (rect.bottom + heightOffset) <= window.innerHeight) ||
+			// The top is above the viewport and the bottom is below the viewport
+			((rect.top + heightOffset) < 0 && (rect.bottom + heightOffset) > window.innerHeight)
+		)
 
-  	}
+	}
 
 
 	function update() {
@@ -42,20 +42,20 @@
 				nodes[i].classList.add("animate-in-done")
 			} else {
 				nodes[i].classList.add("out-of-viewport")
-  		}
-  	}
-  }
+			}
+		}
+	}
 
-	document.addEventListener("DOMContentLoaded", function(event) {
-	  update()
-    // setTimeout(function() {
-    //   document.querySelector("#temp-animate-in").remove()
-    // })
+	document.addEventListener("DOMContentLoaded", function (event) {
+		update()
+		// setTimeout(function() {
+		//   document.querySelector("#temp-animate-in").remove()
+		// })
 	});
 
-	window.addEventListener("scroll", function() {
-	  update()			
-  })
+	window.addEventListener("scroll", function () {
+		update()
+	})
 
 })();
 
